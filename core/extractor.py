5d1263aaf6cfe41fd88b712ecf1ced097e17dd5b
Aman Soni <amansoni.pali001@gmail.com>
2026-08-03 21:27:04 +0530

    implement RAG engine with LangChain integration for transcript question answering

--- core/extractor.py
+++ core/extractor.py
@@ -4,6 +4,7 @@ from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+
 import os 
 
 
